e5b151eeb596a7616b658eb70a40f8787f8e3665
Nishan <aanishan339@gmail.com>
2026-05-05 20:23:22 +0600

    Created review form

---
+++ apps/forms.py
@@ -0,0 +1,16 @@
+from .models import Review
+from django import forms 
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ["review"]
+        widgets = {
+            "review":forms.Textarea(
+                attrs = {
+                    "placeholder":"Write your review here...",
+                    "class":"form-control comment-input",
+                }
+            )
+        }
