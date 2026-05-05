0b0f0a5372c70734c9f70dee8a77a125c7e4ff4f
Nishan <aanishan339@gmail.com>
2026-05-05 20:27:44 +0600

    Wrote urls for all_apps , app_detail , vote views

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,8 @@
-from .views import Home
+from .views import home , all_apps , app_detail , vote
 from django.urls import path
 
 urlpatterns = [
-    path("" , Home , name = "home"),
+    path("all/" , all_apps , name = "all-apps"),
+    path("<str:id>/" , app_detail , name = "app-detail"),
+    path("<str:id>/vote/" , vote , name = "vote"),
 ]
