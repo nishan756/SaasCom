9abf9e6c8fafba2e0dd5409a57e638d19901ec1c
Nishan <aanishan339@gmail.com>
2026-05-06 09:39:40 +0600

    Created authenticate method to authenticate user

--- session/service.py
+++ session/service.py
@@ -1,5 +1,12 @@
 from .repository import UserRepo
+from .exceptions import InvalidForm
 
 class UserService:
     repo = UserRepo()
-    
+
+    def authenticated(self , form , request):
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            return self.repo.authenticated(request, username, password)
+        raise InvalidForm("Invalid form data")
