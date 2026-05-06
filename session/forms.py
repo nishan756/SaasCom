4978193088c847c84fea024446cc13a391b4db43
Nishan <aanishan339@gmail.com>
2026-05-06 09:37:31 +0600

    Created login form class

---
+++ session/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        required = True,
+        label_suffix = "",
+        widget = forms.TextInput(
+            attrs = {
+                "type":"text",
+                "placeholder":"Username",
+                "class":"form-control"
+            }
+        )
+    )
+
+    password = forms.CharField(
+        required = True,
+        label_suffix = "",
+        widget = forms.PasswordInput(
+            attrs = {
+                "type":"password",
+                "placeholder":"Password",
+                "class":"form-control"
+            }
+        )
+    )
