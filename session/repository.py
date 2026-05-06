6294d13473097e1688812ed9271127b890d96781
Nishan <aanishan339@gmail.com>
2026-05-06 09:40:03 +0600

    Created authenticate method to authenticate user

--- session/repository.py
+++ session/repository.py
@@ -1,5 +1,6 @@
 from .models import User , Follow
 from .exceptions import UserNotFound
+from django.contrib.auth import authenticate
 
 class UserRepo:
     queryset = User.objects.all()
@@ -9,6 +10,12 @@ class UserRepo:
             return self.queryset.prefetch_related("followers" , "following").get(username = username)
         except User.DoesNotExist:
             raise UserNotFound("User not found")
+    
+    def authenticated(self , request , username , password):
+        user = authenticate(request , username = username , password = password)
+        if user is not None:
+            return user
+        raise UserNotFound("Invalid username or password")
         
     def add_follower(self , following , follower):
         pass
