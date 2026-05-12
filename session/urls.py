8e31a5c0ed4d062aacef07fcf0106b986a44c011
Nishan <aanishan339@gmail.com>
2026-05-12 20:22:47 +0600

    created url for follow and unfollow

--- session/urls.py
+++ session/urls.py
@@ -1,8 +1,10 @@
-from .views import user_login , user_logout
+from .views import user_login , user_logout , follow , unfollow
 from django.urls import path
 
 
 urlpatterns = [
     path("login" , user_login , name = "login"),
-    path("logout" , user_logout , name = "logout")
+    path("logout" , user_logout , name = "logout"),
+    path("follow/<str:id>/" , follow , name = "follow"),
+    path("unfollow/<str:id>/" , unfollow , name = "unfollow"),
 ]
