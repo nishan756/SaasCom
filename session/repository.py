5cb0c4bb32440a51385e506fdfcfcb9a2ffd0d3e
Nishan <aanishan339@gmail.com>
2026-05-12 20:21:07 +0600

    Added get_user method in UserRepo class

--- session/repository.py
+++ session/repository.py
@@ -5,6 +5,13 @@ from django.contrib.auth import authenticate
 class UserRepo:
     queryset = User.objects.all()
 
+
+    def get_user(self , id):
+        try:
+            return self.queryset.get(id = id)
+        except User.DoesNotExist:
+            raise UserNotFound("User not found")
+
     def view_profile(self , username):
         try:
             return self.queryset.prefetch_related("followers" , "following").get(username = username)
@@ -17,11 +24,18 @@ class UserRepo:
             return user
         raise UserNotFound("Invalid username or password")
         
-    def add_follower(self , following , follower):
-        pass
 
-    def remove_follower(self , following , follower):
-        pass
-    
-    
 
+class FollowRepo:
+    queryset = Follow.objects.all()
+
+    def is_following(self , follower , following):
+        return self.queryset.filter(follower = follower , following = following).exists()
+
+    def follow(self , follower , following):
+        new_follow = Follow(follower = follower , following = following)
+        new_follow.save()
+    
+    def unfollow(self , follower , following):
+        follow = self.queryset.get(follower = follower , following = following)
+        follow.delete()
