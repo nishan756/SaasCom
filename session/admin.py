ab505b126a2365accde58708eef2c9aaacaf18e8
Nishan <aanishan339@gmail.com>
2026-05-13 21:10:43 +0600

    Added bio in UserAdmin

--- session/admin.py
+++ session/admin.py
@@ -28,6 +28,14 @@ class UserAdmin(admin.ModelAdmin):
             },
             
         ),
+
+        (
+            "Bio",
+            {
+                "fields":["bio"],
+            },
+            
+        ),
         
         (
             "User Type",
