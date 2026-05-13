8a75c9b023d930a6696ea32b000a6cee44b65c18
Nishan <aanishan339@gmail.com>
2026-05-13 10:05:45 +0600

    Added url to view user profile

--- session/urls.py
+++ session/urls.py
@@ -1,4 +1,4 @@
-from .views import user_login , user_logout , follow , unfollow
+from .views import user_login , user_logout , follow , unfollow , view_profile
 from django.urls import path
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("logout" , user_logout , name = "logout"),
     path("follow/<str:id>/" , follow , name = "follow"),
     path("unfollow/<str:id>/" , unfollow , name = "unfollow"),
+    path("profile/<str:username>/" , view_profile , name = "profile"),
 ]
