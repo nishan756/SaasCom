2a81dece9715d18dae1ee9755598a0c72c99603c
Nishan <aanishan339@gmail.com>
2026-05-15 23:40:15 +0600

    Created ReportService class

--- session/service.py
+++ session/service.py
@@ -1,5 +1,8 @@
-from .repository import UserRepo , FollowRepo
-from .exceptions import FollowException, InvalidForm
+from .repository import ReportRepo, UserRepo , FollowRepo
+from .exceptions import FollowException, InvalidForm , InvalidContentType
+from .models import User
+from apps.models import App
+from django.contrib.contenttypes.models import ContentType
 
 class UserService:
     repo = UserRepo()
@@ -36,4 +39,22 @@ class FollowService:
             return self.repo.unfollow(follower, following)
         else:
             raise FollowException("You are not following this user.")
-        
+
+class ReportService:
+    repo = ReportRepo()
+    CONTENT_TYPES = {
+        "user":User,
+        "app":App
+    }
+
+
+    def add_report(self , reporter , content_type , id , form):
+        if content_type not in self.CONTENT_TYPES:
+            raise InvalidContentType("Invalid content type.")
+        content_type = ContentType.objects.get_for_model(model = self.CONTENT_TYPES[content_type])
+        if form.is_valid():
+            reason = form.cleaned_data.get("reason" , None)
+            report_type = form.cleaned_data.get("report_type")
+            return self.repo.add_report(reporter = reporter, content_type = content_type, id = id, reason = reason , report_type = report_type)
+        raise InvalidForm("Invalid form data.")
+    
