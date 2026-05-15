8e681886456c2dca7c877d8dbcdbd918b916765c
Nishan <aanishan339@gmail.com>
2026-05-15 23:43:18 +0600

    Added url for report

--- session/urls.py
+++ session/urls.py
@@ -1,4 +1,4 @@
-from .views import user_login , user_logout , follow , unfollow , view_profile
+from .views import user_login , user_logout , follow , unfollow , view_profile , report
 from django.urls import path
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("follow/<str:id>/" , follow , name = "follow"),
     path("unfollow/<str:id>/" , unfollow , name = "unfollow"),
     path("profile/<str:username>/" , view_profile , name = "profile"),
+    path("report/<str:content_type>/<str:id>/" , report , name = "report"),
 ]
