f88b3bb504d9c614df20c69711c3e44272531e4b
Nishan <aanishan339@gmail.com>
2026-05-15 22:18:18 +0600

    Created report form

--- session/forms.py
+++ session/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Report
 
 
 class LoginForm(forms.Form):
@@ -25,3 +26,24 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ["report_type","reason"]
+        widgets = {
+            "report_type" : forms.Select(
+                attrs = {
+                    "class":"form-select",
+                    "required":True
+                }
+            ),
+            "reason":forms.Textarea(
+                attrs = {
+                    "class":"form-control",
+                    "placeholder":"Reason for reporting",
+                    "rows":5,
+                }
+            )
+        }
