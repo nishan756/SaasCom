b962b383d9471d457a62b93017c83812ba1c2a01
Nishan <aanishan339@gmail.com>
2026-05-16 21:26:08 +0600

    Created URL for AppForm template to create app

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,9 @@
-from .views import home , all_apps , app_detail , vote , add_review , del_review
+from .views import create_app, home , all_apps , app_detail , vote , add_review , del_review , create_app
 from django.urls import path
 
 urlpatterns = [
     path("all/" , all_apps , name = "all-apps"),
+    path("create-app/" , create_app , name = "create-app"),
     path("<str:id>/" , app_detail , name = "app-detail"),
     path("<str:id>/vote/" , vote , name = "vote"),
     path("<str:id>/review/add" , add_review , name = "add-review"),
