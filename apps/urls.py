e3d45a75f5d78c03ed657ef07416a99f874c003a
Nishan <aanishan339@gmail.com>
2026-05-17 11:40:08 +0600

    Added URL for delete app

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,11 @@
-from .views import create_app, home , all_apps , app_detail , vote , add_review , del_review , create_app , del_image
+from .views import create_app, del_app ,  all_apps , app_detail , vote , add_review , del_review , create_app , del_image
 from django.urls import path
 
 urlpatterns = [
+    # App Related URLS
     path("all/" , all_apps , name = "all-apps"),
     path("create-app/" , create_app , name = "create-app"),
+    path("del-app/<str:id>" , del_app , name = "del-app"),
 
     # Image related url
     path("del-image/<str:id>" , del_image , name = "del-image"),
