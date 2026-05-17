5ef249d700304173b5e9c9cd1b31e7ecc5181f0a
Nishan <aanishan339@gmail.com>
2026-05-17 10:37:20 +0600

    Added URL for delete image

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,12 @@
-from .views import create_app, home , all_apps , app_detail , vote , add_review , del_review , create_app
+from .views import create_app, home , all_apps , app_detail , vote , add_review , del_review , create_app , del_image
 from django.urls import path
 
 urlpatterns = [
     path("all/" , all_apps , name = "all-apps"),
     path("create-app/" , create_app , name = "create-app"),
+
+    # Image related url
+    path("del-image/<str:id>" , del_image , name = "del-image"),
     path("<str:id>/" , app_detail , name = "app-detail"),
     path("<str:id>/vote/" , vote , name = "vote"),
     path("<str:id>/review/add" , add_review , name = "add-review"),
