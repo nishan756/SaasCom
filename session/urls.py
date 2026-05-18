b48a88fd0a27569a71ed618794ae55434a47ec6e
Nishan <aanishan339@gmail.com>
2026-05-18 19:17:09 +0600

    Added url for users view

--- session/urls.py
+++ session/urls.py
@@ -1,4 +1,4 @@
-from .views import user_login , user_logout , follow , unfollow , view_profile , report , del_report
+from .views import user_login , user_logout , follow , unfollow , view_profile , report , del_report , users
 from django.urls import path
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path("logout" , user_logout , name = "logout"),
     path("follow/<str:id>/" , follow , name = "follow"),
     path("unfollow/<str:id>/" , unfollow , name = "unfollow"),
+    path("users/<str:user_type>/" , users , name = "users"),
     path("profile/<str:username>/" , view_profile , name = "profile"),
     path("report/<str:content_type>/<str:id>/" , report , name = "report"),
     path("report/delete/<str:id>/" , del_report , name = "del-report"),
