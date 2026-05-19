6cb03b8102a4a500c4e5d2c4d367a59e518d9bf3
Nishan <aanishan339@gmail.com>
2026-05-19 11:29:41 +0600

    Create custom Queryset and Mnager for model and added extra index with is_active + job-type

--- jobs/models.py
+++ jobs/models.py
@@ -17,6 +17,46 @@ class JobCategory(models.Model):
     class Meta:
         ordering = ["title"]
 
+class JobQueryset(models.QuerySet):
+
+    def job_type(self , job_type):
+        return self.filter(job_type = job_type)
+    
+    def active_jobs(self):
+        return self.filter(is_active = True)
+    
+    def salary(self , max_salary = None , min_salary = None):
+        if max_salary is not None and min_salary is not None:
+            return self.filter(min_salary__gte = min_salary , max_salary__lte = max_salary)
+        
+        elif min_salary is not None:
+            return self.filter(min_salary__gte = min_salary) 
+        
+        elif max_salary is not None:
+            return self.filter(max_salary__lte = max_salary)
+        return self
+
+    def category(self , category):
+        return self.filter(category__title__iexact = category)
+
+class JobManager(models.Manager):
+    
+    def get_queryset(self):
+        return JobQueryset(model = self.model , using = self._db)
+    
+    def job_type(self , job_type):
+        return self.get_queryset().job_type(job_type = job_type)
+    
+    def active_jobs(self):
+        return self.get_queryset().active_jobs()
+    
+    
+    def salary(self , max_salary = None , min_salary = None):
+        return self.get_queryset().salary(max_salary = max_salary , min_salary = min_salary)
+
+    def category(self , category):
+        return self.get_queryset().category(category = category) 
+    
 
 class Job(models.Model):
     id = models.UUIDField(primary_key = True , default = uuid.uuid4 , editable = False)
@@ -42,6 +82,8 @@ class Job(models.Model):
 
     is_active = models.BooleanField(default = True)
 
+    objects = JobManager()
+
     def __str__(self):
         return f"{self.company} posted a new job "
 
@@ -54,5 +96,7 @@ class Job(models.Model):
             models.Index(fields = ["job_type"]),
             models.Index(fields = ["posted_at"]),
             models.Index(fields = ["category"]),
+            models.Index(fields = ["is_active"]),
+            models.Index(fields = ["is_active" , "job_type"]),
         ]
-
+        ordering = ["-posted_at"]
