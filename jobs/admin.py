6cee5d8ff9965284ff72b8d650eff739961770a5
Nishan <aanishan339@gmail.com>
2026-05-20 18:37:42 +0600

    Registered Job , Category , Application , Currency model

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Job , JobCategory , Currency , Application
+from django.contrib import messages
 
-# Register your models here.
+
+
+@admin.register(JobCategory)
+class JobCategoryAdmin(admin.ModelAdmin):
+    list_display = ["title" , "parent"]
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ["code"]
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ["company" , "category" , "posted_at" , "deadline" , "is_active"]
+    list_filter = ['company' , "is_active" , "category"]
+    actions = ["mark_as_inactive" , "mark_as_active"]
+    list_per_page = 100
+
+    @admin.action(description = "Mark selected jobs as inactive")
+    def mark_as_inactive(self , request , queryset):
+        try:
+            obj_count = queryset.update(is_active = False)
+            messages.success(request , f"{obj_count} job marked as inactive")
+        except Exception as e:
+            messages.error(request , f"An error occured while performing this operation: {str(e)}")
+    
+    @admin.action(description = "Mark selected jobs as active")
+    def mark_as_active(self , request , queryset):
+        try:
+            obj_count = queryset.update(is_active = True)
+            messages.success(request , f"{obj_count} job marked as active")
+        except Exception as e:
+            messages.error(request , f"An error occured while performing this operation:{str(e)}")
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ["job" , "candidate" , "applied_at" , "status"]
+    list_filter = ['job__company' , "job__category"]
+    list_per_page = 100
+
+
