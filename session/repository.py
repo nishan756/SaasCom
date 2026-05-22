4855cd25aa328a334349c265100a794e6743b34a
Nishan <aanishan339@gmail.com>
2026-05-22 20:43:44 +0600

    Removed queryset as class property. So that , we can easily filter objects

--- session/repository.py
+++ session/repository.py
@@ -5,24 +5,22 @@ from django.contrib.auth import authenticate
 from django.contrib.contenttypes.models import ContentType
 
 class UserRepo:
-    queryset = User.objects.all()
 
     def total_user(self):
-        return self.queryset.filter(is_active = True).count()
-
+        return User.objects.filter(is_active = True).count()
 
     def get_user(self , id):
         try:
-            return self.queryset.get(id = id)
+            return User.objects.get(id = id)
         except User.DoesNotExist:
             raise ObjectNotFound("User not found")
     
     def get_users(self , user_type):
-        return self.queryset.prefetch_related("apps" , "followers" , "following").filter(user_type = user_type)
+        return User.objects.prefetch_related("apps" , "followers" , "following").filter(user_type = user_type)
 
     def view_profile(self , username):
         try:
-            return self.queryset.prefetch_related("followers" , "following" , "apps").get(username = username)
+            return User.objects.prefetch_related("followers" , "following" , "apps").get(username = username)
         except User.DoesNotExist:
             raise ObjectNotFound("User not found")
     
@@ -35,31 +33,29 @@ class UserRepo:
 
 
 class FollowRepo:
-    queryset = Follow.objects.all()
 
     def is_following(self , follower , following):
-        return self.queryset.filter(follower = follower , following = following).exists()
+        return Follow.objects.filter(follower = follower , following = following).exists()
 
     def follow(self , follower , following):
         new_follow = Follow(follower = follower , following = following)
         new_follow.save()
     
     def unfollow(self , follower , following):
-        follow = self.queryset.get(follower = follower , following = following)
+        follow = Follow.objects.get(follower = follower , following = following)
         follow.delete()
 
 
 class ReportRepo:
-    queryset = Report.objects.all()
 
     def get_report(self , id):
         try:
-            return self.queryset.get(id = id)
+            return Report.objects.get(id = id)
         except Report.DoesNotExist:
             raise ReportNotFound("Report not found")
     
     def has_user_report(self ,content_type , object_id , reporter):
-        return self.queryset.filter(content_type = content_type , reporter = reporter , object_id = object_id).exists()
+        return Report.objects.filter(content_type = content_type , reporter = reporter , object_id = object_id).exists()
     
     def add_report(self , reporter , report_type , content_type , id , reason = None):
         new_report = Report(
