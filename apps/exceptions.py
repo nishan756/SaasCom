48e465a60d0768609ba9052e2fd05c38471f1898
Nishan <aanishan339@gmail.com>
2026-05-23 15:04:25 +0600

    Rename AlreadyReviewd exception to AlreadyExists , so that we can use this exception class generically

--- apps/exceptions.py
+++ apps/exceptions.py
@@ -1,4 +1,4 @@
 class ObjectNotFound(Exception):pass
-class AlreadyReviewed(Exception):pass
+class AlreadyExists(Exception):pass
 class PermissionDenied(Exception):pass
 class TooManyImage(Exception):pass
