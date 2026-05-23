b7170e603bb00a73ce805c4891bbaf0e5f5816e2
Nishan <aanishan339@gmail.com>
2026-05-23 20:09:15 +0600

    Created url for add_bookmark and del_bookmark. Also change url for add report

--- session/urls.py
+++ session/urls.py
@@ -1,4 +1,4 @@
-from .views import user_login , user_logout , follow , unfollow , view_profile , report , del_report , users
+from .views import user_login , user_logout , follow , unfollow , view_profile , report , del_report , users , add_bookmark , del_bookmark
 from django.urls import path
 
 
@@ -9,6 +9,8 @@ urlpatterns = [
     path("unfollow/<str:id>/" , unfollow , name = "unfollow"),
     path("users/<str:user_type>/" , users , name = "users"),
     path("profile/<str:username>/" , view_profile , name = "profile"),
-    path("report/<str:content_type>/<str:id>/" , report , name = "report"),
-    path("report/delete/<str:id>/" , del_report , name = "del-report"),
+    path("add-report/<str:content_type>/<str:id>/" , report , name = "add-report"),
+    path("del-report/delete/<str:id>/" , del_report , name = "del-report"),
+    path("add-bookmark/<str:content_type>/<str:object_id>" , add_bookmark , name = "add-bookmark"),
+    path("del-bookmark/<str:id>" , del_bookmark , name = "delete-bookmark"),
 ]
