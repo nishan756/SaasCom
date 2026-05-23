ce3a8378b8fe8c8ca8bf08be211a285fb5b4c889
Nishan <aanishan339@gmail.com>
2026-05-23 19:50:58 +0600

    Fixes job query related bugs

--- jobs/service.py
+++ jobs/service.py
@@ -1,25 +1,29 @@
-from .repository import JobRepo
+from .repository import JobRepo , JobCatRepo
 from session.exceptions import InvalidForm
 from django.core.paginator import Paginator
 
 class JobService:
     repo = JobRepo()
 
-    def all_jobs(self , page_num):
-        apps = self.repo.all_jobs()
-        paginator = Paginator(apps , per_page = 15)
-        page_num = page_num
-        apps = paginator.get_page(number = page_num)
-        return self.repo.all_jobs()
+    def all_jobs(self , page_num , **query_set):
+        supported_q_field = ["category" , "job_type" , "experience" , "title"]
+        query_set = {key:value for key , value in query_set.items() if key in supported_q_field}
+        jobs = self.repo.all_jobs(**query_set)
+        paginator = Paginator(jobs , 15)
+        jobs = paginator.get_page(page_num)
+        return jobs
     
     def job_detail(self , id):
         return self.repo.job_detail(id = id)
     
     def post_job(self , company , form):
-       
-        if form.is_valid():
-            job = form.save(commit = False)
-            job.company = company
-            return self.repo.post_job(job)
-        raise InvalidForm(form.errors.as_text())
-    
+       job = form.save(commit = False)
+       job.company = company
+       self.repo.post_job(job)
+        
+
+class JobCatService:
+    repo = JobCatRepo()
+
+    def categories(self):
+        return self.repo.categories()
