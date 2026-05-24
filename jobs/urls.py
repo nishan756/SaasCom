e9d7bb37c96d33cbfb00e97eeae9622cf09b7ff9
Nishan <aanishan339@gmail.com>
2026-05-24 11:54:13 +0600

    Added URL for job application

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,4 +1,4 @@
-from .views import post_job , all_jobs , job_detail
+from .views import post_job , all_jobs , job_detail , apply
 
 from django.urls import path
 
@@ -6,4 +6,7 @@ urlpatterns = [
     path("all/" , all_jobs , name = "all-jobs"),
     path("post-job/" , post_job , name = "post-job"),
     path("job-detail/<str:id>/" , job_detail , name = "job-detail"),
+
+    # Application
+    path("apply/<str:id>/" , apply , name = "apply-job")
 ]
