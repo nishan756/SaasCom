7739a17b14e5c41af2812257129a868e9b2e3f99
Nishan <aanishan339@gmail.com>
2026-05-26 18:06:02 +0600

    Created ReportRepo

---
+++ report/repository.py
@@ -0,0 +1,29 @@
+from .models import Report
+from saas_com.core.exceptions import ObjectNotFound
+
+class ReportRepo:
+
+    def get_report(self , id):
+        try:
+            return Report.objects.get(id = id)
+        except Report.DoesNotExist:
+            raise ObjectNotFound("Report not found")
+    
+    def get_reports(self , content_type , object_id):
+        return Report.objects.filter(content_type = content_type , object_id = object_id)
+    
+    def has_user_report(self ,content_type , object_id , reporter):
+        return Report.objects.filter(content_type = content_type , reporter = reporter , object_id = object_id).exists()
+    
+    def add_report(self , reporter , report_type , content_type , id , reason = None):
+        new_report = Report(
+            reporter = reporter,
+            content_type = content_type,
+            object_id = id,
+            reason = reason if reason else None,
+            report_type = report_type
+        )
+        new_report.save()
+    
+    def del_report(self , report):
+        report.delete()
