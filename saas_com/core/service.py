0bdff340b708aa30dcccc2d88f7dff7d4edd8796
Nishan <aanishan339@gmail.com>
2026-05-26 17:54:49 +0600

    Added generic methods

---
+++ saas_com/core/service.py
@@ -0,0 +1,13 @@
+from django.contrib.contenttypes.models import ContentType
+from .exceptions import InvalidContentType
+from django.utils.http import url_has_allowed_host_and_scheme
+
+def get_content_type(content_type , valid_content_types):
+    if content_type not in valid_content_types:
+        raise InvalidContentType("Invalid content type")
+    return ContentType.objects.get_for_model(valid_content_types[content_type])
+
+def is_safe_url(url , allowed_hosts):
+    if url_has_allowed_host_and_scheme(url , allowed_hosts = allowed_hosts):
+        return url
+    return "/"
