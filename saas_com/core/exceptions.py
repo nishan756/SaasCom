89c25a6bb8e836aa791758f0b3eb26ab0e548802
Nishan <aanishan339@gmail.com>
2026-05-26 17:55:48 +0600

    Added Custom Generic Exception class

---
+++ saas_com/core/exceptions.py
@@ -0,0 +1,8 @@
+class InvalidForm(Exception):pass
+class FollowException(Exception):pass
+class InvalidContentType(Exception):pass
+class InvalidPassword(Exception):pass
+class ObjectNotFound(Exception):pass
+class AlreadyExists(Exception):pass
+class PermissionDenied(Exception):pass
+class TooManyObject(Exception):pass
