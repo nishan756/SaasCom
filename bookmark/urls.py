0eb7de0dac35c6789805d7fb11d2301380fade30
Nishan <aanishan339@gmail.com>
2026-05-29 20:29:23 +0600

    Changed URL structure for add-bookmark. Removed content_type

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -3,6 +3,6 @@ from .views import add_bookmark , del_bookmark
 from django.urls import path
 
 urlpatterns = [
-    path("add-bookmark/<str:content_type>/<str:object_id>" , add_bookmark , name = "add-bookmark"),
+    path("add-bookmark/<str:object_id>" , add_bookmark , name = "add-bookmark"),
     path("del-bookmark/<str:id>" , del_bookmark , name = "del-bookmark"),
 ]
