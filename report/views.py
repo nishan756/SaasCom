d488e49db0ad29ba9ff1ffc6b2a66304bedc9964
Nishan <aanishan339@gmail.com>
2026-05-31 11:01:17 +0600

    Handled Exception in del_report view

--- report/views.py
+++ report/views.py
@@ -13,8 +13,9 @@ from .forms import ReportForm
 
 @require_POST
 @login_required(login_url = "login")
-def report(request , content_type , id):
+def report(request , id):
     HTTP_REFERER = is_safe_url(request.META.get("HTTP_REFERER") , allowed_hosts=request.get_host())
+    content_type = request.POST.get("content_type")
     try:
         report_service.add_report(reporter=request.user, content_type=content_type, id=id, form=ReportForm(request.POST))
         messages.success(request, "Your report has been submitted successfully.")
@@ -37,10 +38,12 @@ def report(request , content_type , id):
 def del_report(request , id):
     HTTP_REFERER = is_safe_url(request.META.get("HTTP_REFERER" , "/") , request.get_host())
     try:
-        report_service.del_report(id = id)
+        report_service.del_report(id = id , user = request.user)
         messages.success(request , "Successfully deleted your report")
     except ObjectNotFound as e:
         messages.error(request , str(e))
     except PermissionDenied as e:
         messages.warning(request , "You can\'t delete this report")
+    except Exception as e:
+        messages.error(request , 'Something went wrong')
     return redirect(HTTP_REFERER)
