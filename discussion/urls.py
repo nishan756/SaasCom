a4ab9403d4bf107db6e373349d2c5a9091a20201
Nishan <aanishan339@gmail.com>
2026-05-31 20:13:28 +0600

    Added URL for update discussion

--- discussion/urls.py
+++ discussion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import post_discussion , delete_discussion , all_discussions , discussion_detail
+from .views import post_discussion , delete_discussion , all_discussions , discussion_detail , update_discussion
 
 urlpatterns = [
     path("" , all_discussions , name = "all-discussions"),
     path("<uuid:id>/" , discussion_detail , name = "discussion-detail"),
     path("post/" , post_discussion , name = "post-discussion"),
+    path("<uuid:id>/update/" , update_discussion , name = "update-discussion"),
     path("<uuid:id>/delete/" , delete_discussion , name = "delete-discussion"),
 ]
