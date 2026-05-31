ea7e03e23497f5b199365027e1b7b6d161ed1688
Nishan <aanishan339@gmail.com>
2026-05-31 11:01:44 +0600

    Changed URL structure of add_report

--- report/urls.py
+++ report/urls.py
@@ -2,6 +2,6 @@ from .views import report , del_report
 from django.urls import path
 
 urlpatterns = [
-    path("add-report/<str:content_type>/<str:id>/" , report , name = "add-report"),
-    path("del-report/delete/<str:id>/" , del_report , name = "del-report"),
+    path("add-report/<str:id>/" , report , name = "add-report"),
+    path("del-report/<str:id>/" , del_report , name = "del-report"),
 ]
