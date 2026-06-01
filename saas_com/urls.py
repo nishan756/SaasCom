c735c2afcca86f8a92508eff07ebe2e4faebb554
Nishan <aanishan339@gmail.com>
2026-06-01 22:31:59 +0600

    Added base URL for comment

--- saas_com/urls.py
+++ saas_com/urls.py
@@ -30,5 +30,6 @@ urlpatterns = [
     path('vote/', include("vote.urls")),
     path('bookmark/', include("bookmark.urls")),
     path('discussion/', include("discussion.urls")),
+    path('comment/', include("comment.urls")),
     path("summernote/" , include("django_summernote.urls"))
 ] + debug_toolbar_urls()
