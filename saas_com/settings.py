569da2d092aea75af7a477c4093a536cfc360c98
Nishan <aanishan339@gmail.com>
2026-06-11 22:12:00 +0600

    Added STATIC_ROOT

--- saas_com/settings.py
+++ saas_com/settings.py
@@ -154,9 +154,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SUMMERNOTE_CONFIG = {
