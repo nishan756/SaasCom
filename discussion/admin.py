94653c8df4310ff8938a7ada2fc87fd588589fda
Nishan <aanishan339@gmail.com>
2026-06-11 22:34:33 +0600

    Registered Tag model in admin

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Discussion
+from .models import Discussion , Tag
 
 
 @admin.register(Discussion)
 class DiscussionAdmin(admin.ModelAdmin):
     list_display = ["user" , "title" , "posted_at"]
     search_fields = ["user__username"]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["title"]
